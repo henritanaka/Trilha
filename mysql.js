7dd7457f1c52dc76b0faa27feacab1a820bd0de7
Henrique Silva <henritanaka10@gmail.com>
2022-11-21 18:03:08 -0300

    Retirando lib nodemon

--- mysql.js
+++ mysql.js
@@ -1,11 +1,21 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    "user" : process.env.MYSQL_USER,
-    "password" : process.env.MYSQL_PASSWORD,
-    "database" : process.env.MYSQL_DATABASE,
-    "host" : process.env.MYSQL_HOST,
-    "port" : process.env.MYSQL_PORT,
+    "user" : "admin2",
+    "password" : "123mudar",
+    "database" : "Trilha03",
+    "host" : "3.87.184.20",
+    "port" : "3306",
     multipleStatements: true
-})      
+}) 
+
+// var mysql = require('mysql');
+// var pool = mysql.createPool({
+//     "user" : process.env.MYSQL_USER,
+//     "password" : process.env.MYSQL_PASSWORD,
+//     "database" : process.env.MYSQL_DATABASE,
+//     "host" : process.env.MYSQL_HOST,
+//     "port" : process.env.MYSQL_PORT,
+//     multipleStatements: true
+// })      
 
 exports.pool = pool;
